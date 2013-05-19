ee5d48fe37403a85e27986016cf11ee113877450
Octav Sandulescu <github@narc.ro>
2013-05-19 18:35:27 +0300

    Early implementation of menus.
    A Menu has a List and does other stuff alongside (e.g. has a border, presumably eventually also a label and other stuff).
    Also, it turns out parent-relative coordinates was the way to go -- this way, you don't have to keep telling your children "Yo, your daddy just moved around a bit!", which wouldn't have worked all that well anyway.

--- game/__init__.py
+++ game/__init__.py
@@ -27,23 +27,23 @@ def main_menu():
     time.sleep(0.2)
     slow_print(top, "Survival is a Hard problem", y=2)
 
-    l = widgets.List(parent=top)
-    l.add_item("New game")
-    l.add_item("Load game", disabled=True)
-    quit_item = l.add_item("Quit")
-    l.hcenter_in_parent()
-    l.vcenter_in_parent()
+    m = widgets.Menu(parent=top)
+    m.add_item("New game")
+    m.add_item("Load game", disabled=True)
+    quit_item = m.add_item("Quit")
+    m.hcenter_in_parent()
+    m.vcenter_in_parent()
 
     quitting = False
     while not quitting:
-        l.render()
+        m.render()
         tcod.flush()
 
         key, mouse = tcod.wait_for_event(tcod.event.KEY_PRESS)
         if tcod.is_window_closed():
             quitting = True
         elif key.vk == tcod.key.ENTER:
-            if l.selected_item is quit_item:
+            if m.selected_item is quit_item:
                 quitting = True
         else:
-            l.handle_event(widgets.event.KEY, key)
+            m.handle_event(widgets.event.KEY, key)
--- game/widgets.py
+++ game/widgets.py
@@ -13,8 +13,11 @@ class Widget(object):
     how to render themselves are widgets.
 
     Widgets may have child widgets; when a widget is told to render itself, it
-    asks its children to render themselves. Child widgets receiving an event
-    should pass that event up to the parent if they don't want to handle it.
+    asks its children to render themselves. Widgets may receive events; if an
+    event is handled by a widget, it should return True. Otherwise, the event
+    should be pased along to children, returning True if one of the children
+    handled it, or False otherwise.
+
     Widgets who are initialized knowing who their parent is will automatically
     register themselves with their parent.
 
@@ -23,9 +26,8 @@ class Widget(object):
     parent's. At render time, a widget that still does not know what its
     console is will assume it is tcod.root_console.
 
-    Coordinates given at init-time are parent-relative, but stored as
-    console-relative (that is, widget.rect.{top,left,center,etc.} are console
-    coordinates).
+    Coordinates (in self.rect) are parent-relative (unless there's no parent,
+    in which case, they are console-relative).
     """
 
     def __init__(self, parent=None, console=None, x=0, y=0, width=0, height=0):
@@ -51,27 +53,54 @@ class Widget(object):
         child.parent = self
         self.children.add(child)
 
+    def point_to_screen(self, point):
+        """ Translates a Point(x,y) inside this widget into screen coordinates """
+        x = point.x + self.rect.left
+        y = point.y + self.rect.top
+
+        if self.parent:
+            return self.parent.point_to_screen(utils.Point(x,y))
+        else:
+            return utils.Point(x,y)
+
+    def screen_to_point(self, point):
+        """
+        Translates a Point(x,y) in screen coordinates into a point inside this
+        widget. Inverse of self.point_to_screen().
+        """
+        x = point.x - self.rect.left
+        y = point.y - self.rect.top
+
+        if self.parent:
+            return self.parent.screen_to_point(utils.Point(x,y))
+        else:
+            return utils.Point(x,y)
+
     def render(self):
         """ The default implementation just asks children to render. """
         for child in self.children:
             child.render()
 
     def handle_event(self, ev, data):
-        """ The default implementation just asks the parent to handle it. """
-        if self.parent is not None:
-            self.parent.handle_event(ev, data)
+        """
+        The default implementation just asks children if they want it. If they
+        return True, they did.
+        """
+        for child in self.children:
+            if child.handle_event(ev, data):
+                return True
+
+        return False
 
     def hcenter_in_parent(self):
         if self.parent is None:
             raise ValueError("Widget trying to hcenter in non-existent parent")
-        self.rect.left = (self.parent.rect.left +
-                          (self.parent.rect.width - self.rect.width) / 2)
+        self.rect.left = (self.parent.rect.width - self.rect.width) / 2
 
     def vcenter_in_parent(self):
         if self.parent is None:
             raise ValueError("Widget trying to vcenter in non-existent parent")
-        self.rect.top = (self.parent.rect.top +
-                         (self. parent.rect.height - self.rect.height) / 2)
+        self.rect.top = (self. parent.rect.height - self.rect.height) / 2
 
 class Label(Widget):
     def __init__(self, parent=None, console=None, x=0, y=0, text="", width=0):
@@ -101,8 +130,9 @@ class Label(Widget):
         self.rect.resize(width=width, height=height)
 
     def render(self):
-        self.console.print_rect_ex(self.rect.left, self.rect.top,
-                                   self.rect.width, self.rect.height,
+        x,y = self.point_to_screen(utils.origin)
+
+        self.console.print_rect_ex(x, y, self.rect.width, self.rect.height,
                                    self.effect, self.align, self._text)
 
 ListItem = collections.namedtuple("ListItem", ["label", "disabled", "on_activate"])
@@ -116,6 +146,7 @@ class List(Widget):
         self.selected_item = None
         self.sub_console = None
         self.scroll_y = 0
+        self.render_item_override = None
 
     def add_item(self, label, disabled=False, on_activate=None):
         """
@@ -184,30 +215,34 @@ class List(Widget):
         draw_width = self.init_width+1 if self.init_width > 0 else self.rect.width
         draw_height = self.init_height if self.init_height > 0 else self.rect.height
 
-        self.console.rect(self.rect.left, self.rect.top, draw_width, draw_height, clear=True)
+        origin = self.point_to_screen(utils.origin)
+        self.console.rect(origin.x, origin.y, draw_width, draw_height, clear=True)
 
         y = 0
-        for o in self.items:
-            height = self.sub_console.get_height_rect(0, y, text=o.label)
+        for i in self.items:
+            height = self.sub_console.get_height_rect(0, y, text=i.label)
 
             bgcolor = tcod.color.BLACK
-            if self.selected_item is o:
+            if self.selected_item is i:
                 bgcolor = tcod.color.AZURE
                 self.scroll_to(y, height)
             self.sub_console.set_default_background(bgcolor)
             self.sub_console.rect(0, y, self.rect.width, height, clear=True)
             self.sub_console.set_default_background(tcod.color.BLACK)
 
-            if o.disabled:
+            if i.disabled:
                 self.sub_console.set_default_foreground(tcod.color.GRAY)
             else:
                 self.sub_console.set_default_foreground(tcod.color.WHITE)
-            self.sub_console.print_rect_ex(0, y, text=o.label)
+
+            self.sub_console.print_rect_ex(0, y, text=i.label)
+            if self.render_item_override:
+                self.render_item_override(self.sub_console, i, 0, y, self.rect.width, height)
             y += height
 
         self.sub_console.blit(0, self.scroll_y,
                               draw_width, draw_height,
-                              self.console, self.rect.left, self.rect.top)
+                              self.console, origin.x, origin.y)
 
     def handle_event(self, ev, data):
         if ev == event.KEY:
@@ -217,3 +252,29 @@ class List(Widget):
             elif data.vk == tcod.key.UP and selected_index > 0:
                 self.selected_item = self.items[selected_index - 1]
 
+class Menu(Widget):
+    def __init__(self, parent=None, console=None, x=0, y=0, width=0, height=0):
+        super(Menu, self).__init__(parent, console, x, y, width, height)
+        self.init_width = width
+        self.init_height = height
+        self.list = List(parent=self, x=x+1, y=y+1, width=max(0, width-2), height=max(0, height-2))
+
+    def add_item(self, label, disabled=False, on_activate=None):
+        item = self.list.add_item(label, disabled, on_activate)
+        self.calc_size()
+        return item
+
+    @property
+    def selected_item(self):
+        return self.list.selected_item
+
+    def calc_size(self):
+        if self.init_width < 1:
+            self.rect.width = self.list.rect.width + 2
+        if self.init_height < 1:
+            self.rect.height = self.list.rect.height + 2
+
+    def render(self):
+        self.console.set_default_foreground(tcod.color.WHITE)
+        self.console.print_frame(self.rect.left, self.rect.top, self.rect.width, self.rect.height)
+        super(Menu, self).render()
