d426c1703c1b5c65367863f7edcab58b2573d31d
Octav Sandulescu <github@narc.ro>
2013-06-30 01:24:54 +0300

    Partial TileMap implementation

--- game/world.py
+++ game/world.py
@@ -1,3 +1,4 @@
+import itertools
 from collections import namedtuple
 from numpy import zeros, int16
 from tcod import color
@@ -43,14 +44,83 @@ def coords_to_chunk(x, y, z):
 
 
 class Tile(object):
-    def __init__(self, name, glyph, fgcolor=color.WHITE, bgcolor=color.BLACK):
+    """
+    A Tile only knows how to draw itself to any position onscreen. It is not
+    intended to be bound to a position in the world (the world instead contains
+    ints that map via tilemap to a Tile instance).
+    """
+    def __init__(self, name, description, glyph, degrades_to='nothing',
+                 fgcolor=color.WHITE, bgcolor=color.BLACK):
         self.name = name
+        self.description = description
         self.glyph = glyph
         self.fgcolor = fgcolor
         self.bgcolor = bgcolor
+        self.degrades_to = degrades_to
 
-tile_dict = dict()
-tile_dict['nothing'] = Tile('Nothing', glyph=' ', bgcolor=color.BLACK)
-tile_dict['floor'] = Tile('Floor', glyph=' ', bgcolor=color.DARK_AMBER)
-tile_dict['wall'] = Tile('Wall', glyph='#', bgcolor=color.DARKER_ORANGE)
+    def __repr__(self):
+        return '<Tile(name=%s)>' % self.name
+
+tiles = dict()
+class TileMap(object):
+    def __init__(self, fp=None):
+        self.degrades = dict()
+        self.tilemap = []
+
+        if fp is not None:
+            # TODO: Load mappings out of fp
+            pass
+        else:
+            self.counter = itertools.count()
+            for tile in tiles.itervalues():
+                self.degrades[tile.name] = tile.degrades_to
+                self.tilemap.append(tile.name)
+
+    # TODO: def __iter__(self)
+
+    def __getitem__(self, key):
+        """ Will return a degraded Tile if the original is not available """
+        return self._get_degraded(self.tilemap[key])
+
+    def _get_degraded(self, tilename):
+        if tilename in tiles:
+            return tiles[tilename]
+        else:
+            return self._get_degraded(self.degrades[tilename])
+
+def _can_degrade(tile):
+    """
+    A tile is considered degradable if it's possible to reach the 'nothing'
+    tile by walking through the tiles.degrades_to references. Note that all
+    three core tiles are degradable.
+    """
+    degrade_chain = set()
+    while tile.degrades_to in tiles:
+        if tile.degrades_to == 'nothing':
+            return True
+        elif tile.degrades_to in degrade_chain:
+            raise Exception("Circular reference: the tile '%s' degrades to '%s', which degrades to it!"
+                            % (tile.name, tile.degrades_to))
+        degrade_chain.add(tile.name)
+        tile = tiles[tile.degrades_to]
+
+    # if we got here, a degrades pointer referenced a nonexistent tile
+    return False
+
+def register_tile(tile):
+    """
+    Tiles may not be registered twice, and they must degrade to something that
+    is, itself, degradable. The core tiles 'nothing', 'basic_floor', and
+    'basic_wall' are always degradable.
+    """
+    if tile.name in tiles:
+        raise KeyError("The tile '%s' has already been registered!" % tile.name)
+    # 'nothing' gets a special pass, as the root of the degrade tree
+    if not _can_degrade(tile) and tile.name != 'nothing':
+        raise ValueError("The tile '%s' is not degradable!" % tile.name)
+    tiles[tile.name] = tile
+
+register_tile(Tile('nothing', 'Nothing', glyph=' ', bgcolor=color.BLACK))
+register_tile(Tile('basic_floor', 'Floor', glyph=' ', bgcolor=color.DARK_AMBER))
+register_tile(Tile('basic_wall', 'Wall', glyph='#', bgcolor=color.DARKER_ORANGE))
 
